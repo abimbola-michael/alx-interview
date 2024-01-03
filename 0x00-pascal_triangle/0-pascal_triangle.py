67b87d59563415fa7e2e91790577b9ec7bc84e76
Abimbola Michael <abimbolamichael100.com>
2024-01-03 18:40:53 +0100

    Pascal Triangle tasks

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n"""
+
+    if n <= 0:
+        return []
+    
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            
+        if i > 0:
+            row.append(1)
+        
+        triangle.append(row)
+        
+    return triangle
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+A Pascal Triangle Algorithm
