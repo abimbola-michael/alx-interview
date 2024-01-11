82c31eaba5e9ea4a2c59c373c452d9acbf1afd4a
Abimbola Michael <abimbolamichael100.com>
2024-01-11 20:11:33 +0100

    Lockboxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened """
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    keys.extend(list(set(boxes[0])))
+    
+    index = 1
+    while index < len(keys):
+        key = keys[index]
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes) and box > 0:
+                keys.append(box)
+        index += 1
+    
+    if len(keys) == len(boxes):
+        return True
+    return False
