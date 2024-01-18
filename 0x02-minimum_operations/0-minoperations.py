2d95578dd7efe8dfbb19d27adae68dcd76a81342
Abimbola Michael <abimbolamichael100.com>
2024-01-18 19:41:08 +0100

    Update

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+# In a text file, there is a single character H.
+#Your text editor can execute only two operations
+#in this file: Copy All and Paste. Given a number n, write a
+#method that calculates the fewest number of operations
+#needed to result in exactly n H characters in the file.
+
+# Prototype: def minOperations(n)
+# Returns an integer
+# If n is impossible to achieve, return 0
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    # Find the smallest factor greater than 1
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+
+    return n
