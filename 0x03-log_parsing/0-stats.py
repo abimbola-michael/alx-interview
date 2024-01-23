6acfec2c1326853bcf0a1e67eeaa3e67412bb846
Abimbola Michael <abimbolamichael100.com>
2024-01-23 17:33:55 +0100

    LOg parsing task update

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Write a script that reads stdin line by line and computes metrics:"""
+
+import sys
+
+
+def print_stats(status_codes, file_size):
+    """Prints the stats"""
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print("{}: {}".format(key, value))
+
+
+def main():
+    """
+    Reads stdin line by line and computes metrics:
+    """
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+    total_size = 0
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            try:
+                words = line.split()
+                file_size = int(words[-1])
+                status_code = words[-2]
+                total_size += file_size
+                if status_code in status_codes.keys():
+                    status_codes[status_code] += 1
+                line_count += 1
+
+                if line_count % 10 == 0:
+                    print_stats(status_codes, total_size)
+
+            except (IndexError, ValueError):
+                pass
+    except KeyboardInterrupt:
+        print_stats(status_codes, total_size)
+    
+if __name__ == "__main__":
+    main()
