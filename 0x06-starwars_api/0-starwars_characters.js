61fe0fa33fb6a3007a13aa436e2d2b7b77684a8c
Abimbola Michael <abimbolamichael100.com>
2024-02-15 16:42:00 +0100

    Corrrection:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,31 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    getCharacterName(characters, 0);
-  }
-});
-
-function getCharacterName(characters, index) {
+async function getCharacterName (characters, index) {
   if (index === characters.length) return;
   const characterPath = characters[index];
   request(characterPath, (error, response, body) => {
     if (error) {
       console.log(error);
+    } else if (response.statusCode !== 200) {
+      console.error('Unexpected status code:', response.statusCode);
     } else {
       console.log(JSON.parse(body).name);
       getCharacterName(characters, index + 1);
     }
   });
 }
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+  } else {
+    const characters = JSON.parse(body).characters;
+    getCharacterName(characters, 0);
+  }
+});
