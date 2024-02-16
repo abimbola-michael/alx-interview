9f108cc3e16427d84b69c11558a802627315a6d2
Abimbola Michael <abimbolamichael100.com>
2024-02-16 11:44:28 +0100

    Correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-async function getCharacterName (characters, index) {
+async function getCharacterName (characters, index, names = []) {
   if (index === characters.length) {
+    names.forEach((name) => console.log(name));
     return;
   }
   const characterPath = characters[index];
@@ -15,8 +16,9 @@ async function getCharacterName (characters, index) {
     } else if (response.statusCode !== 200) {
       console.error('Unexpected status code:', response.statusCode);
     } else {
-      console.log(JSON.parse(body).name);
-      getCharacterName(characters, index + 1);
+      // console.log(JSON.parse(body).name);
+      names.push(JSON.parse(body).name);
+      getCharacterName(characters, index + 1, names);
     }
   });
 }
@@ -24,13 +26,16 @@ if (process.argv.length !== 3) {
   console.log('Usage: ./0-starwars_characters.js <filmId>');
   process.exit(1);
 }
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-  } else {
-    const characters = JSON.parse(body).characters;
-    getCharacterName(characters, 0);
-  }
-});
+async function getMovies () {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode !== 200) {
+      console.error('Unexpected status code:', response.statusCode);
+    } else {
+      const characters = JSON.parse(body).characters;
+      getCharacterName(characters, 0);
+    }
+  });
+}
+getMovies();
