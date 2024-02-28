2bb39bbdb0e009dda78412d58d655ce76c625c17
Abimbola Michael <abimbolamichael100.com>
2024-02-28 15:27:01 +0100

    Trying to improve the efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,9 +12,11 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    dp = [0] + [float('inf')] * total
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] > total else -1
