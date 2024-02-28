1c99a255d6bf0c9642fa17c1ac47d3d0032e5952
Abimbola Michael <abimbolamichael100.com>
2024-02-28 17:30:19 +0100

    Update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,11 +12,12 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    dp = [total + 1] * (total + 1)
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return -1 if dp[total] > total else dp[total]
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return -1 if dp[total] == float('inf') else dp[total]
